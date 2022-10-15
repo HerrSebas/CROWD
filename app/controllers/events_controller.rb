4215207fc880242985a0c99846cf7940b9361900
Sebastian Naranjo <sebnartor@gmail.com>
2022-10-15 15:21:43 -0500

    created CRUD for Event model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @flat.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, status: :see_other
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:event_date, :event_location, :event_capacity,
+    :event_name, :event_latitude, :event_longitude, :event_price)
+  end
 end
