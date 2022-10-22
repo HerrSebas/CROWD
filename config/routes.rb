93abd4d31c41853a748da363cfcf720294c74efe
Mayra Moreno <maleja13p@gmail.com>
2022-10-22 17:04:08 -0500

    search bar y esqueleto cards de events

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/pages/profile", to: "pages#profile"
+  get "/search", to: "events#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
