16d07a97ff6e8240bce5c6a2b48a34c752e328d2
santiago munoz <santiagoml2@hotmail.com>
2022-10-29 16:19:39 -0500

    replaced event_location for address on Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,12 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    @markers = @flats.geocoded.map do |flat|
+    @markers = @events.geocoded.map do |event|
       {
-        lat: flat.latitude,
-        lng: flat.longitude
+        lat: event.latitude,
+        lng: event.longitude
       }
+    end
   end
 
   def show
