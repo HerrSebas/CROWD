1faae021c13314a022bbb8bcd6ab0669ee71cd68
santiago munoz <santiagoml2@hotmail.com>
2022-10-29 15:59:11 -0500

    Geocoding set up

--- db/schema.rb
+++ db/schema.rb
@@ -44,11 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_005330) do
 
   create_table "events", force: :cascade do |t|
     t.date "event_date"
-    t.string "event_location"
+    t.string "address"
     t.integer "event_capacity"
     t.string "event_name"
-    t.float "event_latitude"
-    t.float "event_longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.integer "event_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
