414e99c59fb8dc4068eb2779ad7a2f325897f8be
santiago munoz <santiagoml2@hotmail.com>
2022-10-29 14:26:00 -0500

    added geocoding config to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   has_many :tickets
   has_one_attached :photo
   has_many :users
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
