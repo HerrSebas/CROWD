6431f8af303a08d4203e27a519b93777d9da2d5a
Sebastian Naranjo <sebnartor@gmail.com>
2022-10-29 21:22:34 -0500

    d

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "sebnartor@gmail.com",
+  address: "smtp.gmail.com",
   port: 587,
   domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  user_name: ENV.fetch('GMAIL_ADDRESS'),
+  password: ENV.fetch('GMAIL_APP_PASSWORD'),
   authentication: :login,
   enable_starttls_auto: true
 }
