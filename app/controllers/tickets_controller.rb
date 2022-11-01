af24064c59db66189290136e51a2c7f65ba2e989
Sebastian Naranjo <sebnartor@gmail.com>
2022-10-31 21:02:48 -0500

    changed https

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,7 +5,7 @@ class TicketsController < ApplicationController
     @tickets = Ticket.where(user: current_user)
     @codes = []
     @tickets.each do |ticket|
-      code = RQRCode::QRCode.new("localhost:3000/orders/#{ticket.order.id}/tickets/#{ticket.id}")
+      code = RQRCode::QRCode.new("https://crowd-912.herokuapp.com/orders/#{ticket.order.id}/tickets/#{ticket.id}")
       svg = code.as_svg(
         color: "000",
         shape_rendering: "crispEdges",
